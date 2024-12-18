97c0e7643842e3855601d1824a06d9b9109e0afc
thedonkadi <dykumar04@gmail.com>
2024-12-18 15:48:35 +0530

    Initial commit with project files

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDH1a1EM0g4cvtTRObAc_m3wguhwPsNI78",
+    authDomain: "mocks-76624.firebaseapp.com",
+    databaseURL: "https://mocks-76624-default-rtdb.firebaseio.com",
+    projectId: "mocks-76624",
+    storageBucket: "mocks-76624.firebasestorage.app",
+    messagingSenderId: "386868958240",
+    appId: "1:386868958240:web:491bbb47d0e296f08f9845",
+    measurementId: "G-RT25YYV2X9"
+  };
+  
